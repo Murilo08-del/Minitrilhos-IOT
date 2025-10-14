88bdcf206fd1ccd740dec67c6cb230fcce6e4770
Murilo08-del <murilo_h_da-costa@estudante.sesisenai.org.br>
2025-10-14 12:53:58 -0300

    S1

--- S1/S1.ino
+++ S1/S1.ino
@@ -6,6 +6,7 @@ PubSubClient mqtt(client);  //cria objeto p/ mqtt usando WiFi
 
 const String brokerURL = "test.mosquitto.org";
 const int brokerPort = 1883;
+const String topico = "TopicoMurilo"; //nome topico 
 
 const String brokerUser = "";  //variável para o user do brocker
 const String brokerPass = "";  //variável para a senha do brocker
@@ -28,23 +29,41 @@ void setup() {
   //Configura a placa para mostra na tela
   mqtt.setServer(brokerURL.c_str(), brokerPort);
   Serial.println("Conectando no Broker");
-  
-  String boardID = "S1-"; //Cria um nome que começa com "s1-"
-  boardID += String(random(0xffff),HEX); //Junta o "s1-" com um número aleatório Hexadecimal
+
+  String boardID = "S1-";                  //Cria um nome que começa com "s1-"
+  boardID += String(random(0xffff), HEX);  //Junta o "s1-" com um número aleatório Hexadecimal
 
   //Enquanto não estiver conectado mostra "."
-  while (!mqtt.connect(boardID.c_str())){
+  while (!mqtt.connect(boardID.c_str())) {
     Serial.print(".");
     delay(200);
   }
+  mqtt.subscribe(topico.c_str());  
+  mqtt.setCallback(callback);
   Serial.println("\nConectado com sucesso ao broker");
 }
-
 void loop() {
-  String msg = "Murilo: Oi"; //Informação que será enviada para o broker
+  //mqtt.publish(topico.c_str(), msg.c_str());
+  //delay(1000);
+  //Serial.println(msg);  
+  //mqtt.loop();
+  String msg = "";  //Informação que será enviada para o broker
   String topico = "AulaIoT/msg";
-  mqtt.publish(topico.c_str(), msg.c_str());
-  delay(1000);
-  Serial.println(msg);
-  mqtt.loop();
+  if (Serial.available() > 0) {
+    msg = Serial.readStringUntil('/n');
+    Serial.print("Mensagem digitada: ");
+    Serial.println(msg);
+    msg = "Murilo:" + msg;
+    mqtt.publish("TopicoLuiz", msg.c_str());  //envia ms
+  }
+  mqtt.loop();  //mantem a conexao
+}
+//abaixo do loop
+void callback(char* topic, byte* payload, unsigned long length) {
+  String msgRecebida = "";
+  for (int i = 0; i < length; i++) {
+    msgRecebida += (char) payload[i];
+  }
+  Serial.println(msgRecebida);
+  // fazer o controle aqui 
 }
